5015cd39246594641b4d28d0b4a8d5ceb28623a4
Georgijs Milancejs <georgijs.milancejs@gmail.com>
2025-10-12 13:34:59 +0300

    Added program RepeatingBreakingAndRemembering.java

---
+++ src/main/java/rvt/RepeatingBreakingAndRemembering.java
@@ -0,0 +1,46 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class RepeatingBreakingAndRemembering {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give numbers:");
+
+        int summ = 0;
+        int count = 0;
+        double avg = 0;
+        int even = 0;
+        int odd = 0;
+
+        // We don't know how many times the user will enter numbers,
+        // so we create a for loop that runs indefinitely.
+        for (;;) {
+            int number = scanner.nextInt();
+
+            if (number == -1) {
+                break;
+            }
+            
+            summ += number;
+            count++;
+            avg = (double) summ / count;
+
+            if (number % 2 == 0) {
+                even += 1;
+            } else {
+                odd += 1;
+            }
+        
+        }
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + summ);
+        System.out.println("Numbers: " + count);
+        System.out.println("Average: " + avg);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+
+        scanner.close();
+    }
+}
