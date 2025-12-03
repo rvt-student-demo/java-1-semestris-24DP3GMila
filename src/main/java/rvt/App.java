f34a352bab759b147fdeeff5d40c3c469a76308d
Georgijs Milancejs <georgijs.milancejs@gmail.com>
2025-12-03 19:52:38 +0200

    Box.java

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,19 +1,20 @@
 package rvt;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int Number;
+        Box box = new Box(2.5, 5.0, 6.0);
 
-        while (true) {
-            System.out.println("Give a number:");
-            Number = scanner.nextInt();
-            if (Number == 4) {
-                break;
-            }
-        }
-        scanner.close();
+        System.out.println("Area: " + box.area() + " Volume: " + box.volume());
+
+        Box bigger = box.biggerBox(box);
+        System.out.println("Bigger box: ");
+        System.out.println("Width: " + bigger.width() + " Height: " + bigger.height() + " Length: " + bigger.length());
+
+        Box smaller = box.smallerBox(box);
+        System.out.println("Smaller box");
+        System.out.println("Width: " + smaller.width() + " Height: " + smaller.height() + " Length: " + smaller.length());
+
+        System.out.println("Does small box nest inside big box? " + smaller.nests(bigger));
+        System.out.println("Does big box nest inside small box? " + bigger.nests(smaller));
     }
 }
---
+++ src/main/java/rvt/Box.java
@@ -0,0 +1,79 @@
+package rvt;
+
+public class Box {
+    private double width;
+    private double height;
+    private double length;
+
+    public Box(double width, double height, double length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public Box(double side) {
+        this.width = side;
+        this.height = side;
+        this.length = side;
+    }
+
+    public Box(Box oldBox) {
+        this.width = oldBox.width;
+        this.height = oldBox.height;
+        this.length = oldBox.length;
+    } 
+
+    public double volume() {
+        return width * height * length;
+    }
+
+    public double area() {
+        return 2 * (width * height * length + height * length);
+    }
+
+    public double length() {
+        return length;
+    }
+
+    public double height() {
+        return height;
+    }
+
+    public double width() {
+        return width;
+    }
+
+    private double faceArea() {
+        return width * height;
+    }
+
+    private double topArea() {
+        return width * length;
+    }
+
+    private double sideArea() {
+        return height * length;
+    }
+
+    public Box biggerBox(Box oldBox) {
+        return new Box(
+            1.25 * oldBox.width(),
+            1.25 * oldBox.height(),
+            1.25 * oldBox.length()
+        );
+    }
+
+    public Box smallerBox(Box oldBox) {
+        return new Box(
+            0.75 * oldBox.width(),
+            0.75 * oldBox.height(),
+            0.75 * oldBox.length()
+        );
+    }
+
+    public boolean nests(Box outsideBox) {
+        return this.height < outsideBox.height &&
+               this.width < outsideBox.width &&
+               this.length < outsideBox.length;
+    }
+}
