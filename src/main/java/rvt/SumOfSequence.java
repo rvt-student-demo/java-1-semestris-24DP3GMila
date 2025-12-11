4e587fad60ab279c57cfb7ad4bb34b27ed603dc6
Georgijs Milancejs <georgijs.milancejs@gmail.com>
2025-12-11 16:57:07 +0200

    SumOfSequence.java

--- src/main/java/rvt/SumOfSequence.java
+++ src/main/java/rvt/SumOfSequence.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class SumOfSequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Ask user for input
         System.out.println("Last number?");
         int n = scanner.nextInt();
 
-        int summ = 0; // variable to store the sum
+        int summ = 0;
 
         // Use a loop to add numbers from 1 to n
         for (int i = 1; i <= n; i++) {
             summ += i; // same as sum = sum + i
         }
-
         // Print the result in the same format as the example
         System.out.println("The sum is " + summ);
 
