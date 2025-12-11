8b3eb76f5c652d20715713a7f1d31c39534eb304
Georgijs Milancejs <georgijs.milancejs@gmail.com>
2025-12-11 16:41:37 +0200

    SecondsInADay.java

--- src/main/java/rvt/SecondsInADay.java
+++ src/main/java/rvt/SecondsInADay.java
@@ -2,13 +2,13 @@ package rvt;
 
 import java.util.Scanner;
 
-public class Seconds {
+public class SecondsInADay {
  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("How many days would you like to convert into seconds?");
     int number = Integer.valueOf(scanner.nextLine());
-    int calculation = number * 60 * 60 * 24;
+    int calculation = number * 60 * 60 * 24; //formula to calculate seconds into days
     System.out.println(calculation);
 
     scanner.close();
