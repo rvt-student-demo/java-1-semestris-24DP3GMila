8a439a6ac7b634ada5e2382fd9a748d74af9a795
Georgijs Milancejs <georgijs.milancejs@gmail.com>
2025-12-11 16:58:53 +0200

    SumOfSequenceTheSequel.java

--- src/main/java/rvt/SumOfSequenceTheSequel.java
+++ src/main/java/rvt/SumOfSequenceTheSequel.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class SumOfSequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("First number?");
         int num_first = scanner.nextInt();
         System.out.println("Last number?");
         int num_last = scanner.nextInt();
-
         int summ = 0;
 
         for (int i = num_first; i <= num_last; i++) {
