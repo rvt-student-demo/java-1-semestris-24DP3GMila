a493c99d980aabc418a19366d5da5c4306573a94
Georgijs Milancejs <georgijs.milancejs@gmail.com>
2025-12-11 17:14:12 +0200

    PaymentCard.java

--- src/main/java/rvt/PaymentCard.java
+++ src/main/java/rvt/PaymentCard.java
@@ -1,49 +1,49 @@
 package rvt;
 
 public class PaymentCard {
-    private double balance;
+    private double atlikums;
 
     public PaymentCard(double openingBalance) {
-        this.balance = openingBalance;
+        this.atlikums = openingBalance;
     }
 
     public void to_string() {
-        System.out.println("The card has a balance of " + String.format("%.2f", balance) + " euros");
+        System.out.println("The card has a balance of " + String.format("%.2f", atlikums) + " euros");
     }
 
     public void eat_affordably() {
-        balance -= 2.60;
+        atlikums -= 2.60;
     }
 
     public void eat_heartily() {
-        balance -= 4.60;
+        atlikums -= 4.60;
     }
 
     public void add_money(double amount) {
-        balance += amount;
+        atlikums += amount;
     }
 
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(50);
-        System.out.println(card);
+        PaymentCard karte = new PaymentCard(50);
+        System.out.println(karte);
 
-        card.eat_affordably();
-        card.to_string();
+        karte.eat_affordably();
+        karte.to_string();
 
-        card.eat_heartily();
-        card.to_string();
+        karte.eat_heartily();
+        karte.to_string();
 
-        card.add_money(15);
-        card.to_string();
+        karte.add_money(15);
+        karte.to_string();
 
-        card.add_money(10);
-        card.to_string();
+        karte.add_money(10);
+        karte.to_string();
 
-        card.add_money(200);
-        card.to_string();
+        karte.add_money(200);
+        karte.to_string();
 
-        card.eat_affordably();
-        card.eat_heartily();
-        card.to_string();
+        karte.eat_affordably();
+        karte.eat_heartily();
+        karte.to_string();
     }
 }
